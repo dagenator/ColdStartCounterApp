b71bdbf9c7e264a3e70560152fe0f56b819cfec1
Darina <37955869+dagenator@users.noreply.github.com>
2022-06-24 16:57:47 +0500

    теперь инициаллизация приложения на холодном старте учитывается без загрузки main activity

--- app/src/main/java/com/example/coldstarttestapp/MainActivity.kt
+++ app/src/main/java/com/example/coldstarttestapp/MainActivity.kt
@@ -5,57 +5,49 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
 import com.example.coldstarttestapp.data.app.MyApp
-import com.example.coldstarttestapp.data.db.ColdStart
 import com.example.coldstarttestapp.data.db.MainDataBase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.text.DateFormat
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val isColdStart = MyApp.fromApplication
 
         CoroutineScope(Dispatchers.IO).launch {
-            val count = workWithBD(isColdStart)
+            val count = getCountFromBd()
             withContext(Dispatchers.Main){
                 showToastWithCount(count)
             }
         }
     }
 
-    fun showToastWithCount(countInt: Int) {
+    fun showToastWithCount(countInt: Int?) {
         if(countInt == 3){
             val toast = Toast.makeText(
                 applicationContext,
                 "Cold start number $countInt",
                 Toast.LENGTH_SHORT
             )
-
+            toast.show()
+        }else if(countInt == null ){
+            val toast = Toast.makeText(
+                applicationContext,
+                "Error with local bd",
+                Toast.LENGTH_SHORT
+            )
             toast.show()
         }
     }
 
-    private fun getCurrentTime(): String {
-        return DateFormat.getDateInstance().format(Date())
-    }
-
-    private suspend fun workWithBD(isColdStart:Boolean): Int {
-        val db = Room.databaseBuilder(
-            applicationContext,
-            MainDataBase::class.java, "database-name"
-        ).build()
-
-        val coldStartDao = db.ColdStartDao()
-        val count = coldStartDao.getColdStartCount() + 1
-        if (isColdStart) {
-            coldStartDao.insertColdStart(ColdStart(count, getCurrentTime()))
+    private fun getCountFromBd(): Int? {
+        MyApp.db?.let {
+            val coldStartDao = it.ColdStartDao()
+            return coldStartDao.getColdStartCount()
         }
-        return count
+        return null
     }
 }
